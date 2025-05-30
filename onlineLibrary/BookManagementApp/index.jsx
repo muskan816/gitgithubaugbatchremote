6933759c2385c64ac591803fdb6b86d7c0ce380a
muskan816 <muskmane420@gmail.com>
2025-05-30 15:06:45 +0530

    Add files via upload

---
+++ onlineLibrary/BookManagementApp/index.jsx
@@ -0,0 +1,14 @@
+// index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store"; // Import the store
+import App from "./App"; // Import the main App component
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
